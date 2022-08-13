c95824b31c46c19c6ff651662578311889dfb317
Mathias Larrosa <motaico4@gmail.com>
2022-08-13 09:44:09 -0300

    Terminado el decodificador, aparece y desaparesen imagenes dependiendo si se resolvio un puzzle y guira para la derecha o izquierda dependendo el input.

--- Assets/Scripts/CodeWheelController.cs
+++ Assets/Scripts/CodeWheelController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CodeWheel : MonoBehaviour
+public class CodeWheelController : MonoBehaviour
 {
-    public Image topPart, downPart;
+    public Image topPart;
+    public List<Image> downPartImages = new List<Image>();
     public float gradesToRotate = 13.8f;
 
     private void Start()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
@@ -26,4 +27,5 @@ public class CodeWheel : MonoBehaviour
                 new Vector3(0, 0, -1 * gradesToRotate * Time.deltaTime));
         }
     }
+
 }
---
+++ Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public CodeWheelController codeWheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PuzzleSolved();
+    }
+
+    public void PuzzleSolved()
+    {
+        if (GameObject.Find("PuzzleEj").GetComponent<Puzzle>().isSolved)
+        {
+            codeWheel.downPartImages[0].enabled = true;
+        }
+        if (GameObject.Find("PuzzleEj (1)").GetComponent<Puzzle>().isSolved)
+        {
+            codeWheel.downPartImages[1].enabled = true;
+        }
+        if (GameObject.Find("PuzzleEj (2)").GetComponent<Puzzle>().isSolved)
+        {
+            codeWheel.downPartImages[2].enabled = true;
+        }
+        if (GameObject.Find("PuzzleEj (3)").GetComponent<Puzzle>().isSolved)
+        {
+            codeWheel.downPartImages[3].enabled = true;
+        }
+    }
+}
