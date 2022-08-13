5ba864f09b2f1a082642d5bb33cd83fbba65eb42
Mathias Larrosa <motaico4@gmail.com>
2022-08-13 01:02:17 -0300

    ﻿Un par de imagenes mal recortadas y el script del code wheel.

---
+++ Assets/Scripts/CodeWheel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeWheel : MonoBehaviour
+{
+    public Image topPart, downPart;
+    public float gradesToRotate = 13.8f;
+
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            topPart.GetComponent<RectTransform>().Rotate(
+                new Vector3(0, 0, 1 * gradesToRotate * Time.deltaTime));
+        }
+    }
+}
