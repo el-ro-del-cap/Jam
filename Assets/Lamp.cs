3edf2ed545e6979b12797b2242c3565cd21c5ebf
el-ro-del-cap <rodri5202@hotmail.com>
2022-08-14 16:35:51 -0300

    El merge

--- Assets/Lamp.cs
+++ Assets/Lamp.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 
 public class Lamp : MonoBehaviour
 {
+    public Text WarningText;
+    private PuzzleManager adminPuz;
     public GameObject Aparicion;
     private AudioSource Sonoro;
+    public GameObject LampObject;
+    public Sprite LampOn;
+    public Sprite LampOff;
     // Start is called before the first frame update
     void Awake()
     {
         Sonoro = gameObject.GetComponent<AudioSource>();
+        adminPuz = FindObjectOfType<PuzzleManager>();
     }
 
     // Update is called once per frame
@@ -21,10 +27,25 @@ public class Lamp : MonoBehaviour
 
     public void EventoMistico()
     {
-        if (Aparicion.activeInHierarchy == true)
-            Aparicion.SetActive(false);
-        else
-            Aparicion.SetActive(true);
-        Sonoro.Play();
+        switch (adminPuz.hasLightBlob)
+        {
+            case true:
+                if (Aparicion.activeInHierarchy == true)
+                {
+                    Aparicion.SetActive(false);
+                    LampObject.GetComponent<Image>().sprite = LampOn;
+                }
+                else
+                {
+                    Aparicion.SetActive(true);
+                    LampObject.GetComponent<Image>().sprite = LampOff;
+                }
+                Sonoro.Play();
+                break;
+            case false:
+                WarningText.text = "I need a light bulb.";
+                Sonoro.Play();
+                break;
+        }
     }
 }
